1f7fb333eea6cd8da135ce172b7fae9e35b04bd2
fangJinliang1 <fangjinliang1@huawei.com>
2022-05-06 17:52:01 +0800

    slot update common event
    Signed-off-by: fangJinliang1 <fangjinliang1@huawei.com>
    Change-Id: Ide2f8e6d56ee9a436c5d547145996b829c7236c0
    Signed-off-by: fangJinliang1 <fangjinliang1@huawei.com>

--- interfaces/innerkits/native/include/common_event_support.h
+++ interfaces/innerkits/native/include/common_event_support.h
@@ -1055,16 +1055,22 @@ public:
     static const std::string COMMON_EVENT_NITZ_TIMEZONE_CHANGED;
 
     /**
-     * Indicate the action of a common event that a new sms wappush has been received by the device.
+     * Indicates the action of a common event that a new sms wappush has been received by the device.
      * This is a protected common event that can only be sent by system.
      */
     static const std::string COMMON_EVENT_SMS_WAPPUSH_RECEIVE_COMPLETED;
 
     /**
-     * Indicate the action of a common event that the operator config has been updated.
+     * Indicates the action of a common event that the operator config has been updated.
      * This is a protected common event that can only be sent by system.
      */
     static const std::string COMMON_EVENT_OPERATOR_CONFIG_CHANGED;
+
+    /**
+     * Indicates the action of a common event that the notification slot has been updated.
+     * This is a protected common event that can only be sent by system.
+     */
+    static const std::string COMMON_EVENT_SLOT_CHANGE;
 
     /**
      * Only for test case.
