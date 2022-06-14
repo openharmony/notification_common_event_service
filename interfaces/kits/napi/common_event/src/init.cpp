1b8db1e40f1c75525a9a6f7b41fc5fc5116303de
fangJinliang1 <fangjinliang1@huawei.com>
2022-06-14 20:08:02 +0800

    mode name modify
    Signed-off-by: fangJinliang1 <fangjinliang1@huawei.com>
    Change-Id: Ibb7cbfbacd290b64c391735938f98b522ff51c53

--- interfaces/kits/napi/common_event/src/init.cpp
+++ interfaces/kits/napi/common_event/src/init.cpp
@@ -46,7 +46,7 @@ static napi_module _module = {
     .nm_flags = 0,
     .nm_filename = nullptr,
     .nm_register_func = Init,
-    .nm_modname = "commonevent",
+    .nm_modname = "commonEvent",
     .nm_priv = ((void *)0),
     .reserved = {0}
 
