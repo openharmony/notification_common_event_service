081631f0ba59e65ae27e782ddf2b776ce72b2654
fangJinliang1 <fangjinliang1@huawei.com>
2023-03-07 20:47:35 +0800

    fix tdd
    Signed-off-by: fangJinliang1 <fangjinliang1@huawei.com>
    Change-Id: I02db0e090cbe387dd6c8e76b6bd65e2cac2ce590
    Signed-off-by: fangJinliang1 <fangjinliang1@huawei.com>

--- frameworks/native/src/common_event_subscriber.cpp
+++ frameworks/native/src/common_event_subscriber.cpp
@@ -20,18 +20,18 @@ namespace OHOS {
 namespace EventFwk {
 CommonEventSubscriber::CommonEventSubscriber() : result_(nullptr)
 {
-    EVENT_LOGI("constructor CommonEventSubscriber: %{private}p", this);
+    EVENT_LOGI("constructor CommonEventSubscriber");
 }
 
 CommonEventSubscriber::CommonEventSubscriber(const CommonEventSubscribeInfo &subscribeInfo)
     : subscribeInfo_(subscribeInfo), result_(nullptr)
 {
-    EVENT_LOGI("constructor CommonEventSubscriber: %{private}p", this);
+    EVENT_LOGI("constructor CommonEventSubscriber");
 }
 
 CommonEventSubscriber::~CommonEventSubscriber()
 {
-    EVENT_LOGI("destructor CommonEventSubscriber: %{private}p", this);
+    EVENT_LOGI("destructor CommonEventSubscriber");
 }
 
 const CommonEventSubscribeInfo &CommonEventSubscriber::GetSubscribeInfo() const
