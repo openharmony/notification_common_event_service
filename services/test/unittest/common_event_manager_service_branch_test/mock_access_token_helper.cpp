a4ec8187a1b82f734918374824c7a84486d599b6
liuyanzhi <liuyanzhi3@huawei.com>
2023-03-23 08:45:38 +0000

    add tdd
    Signed-off-by: liuyanzhi <liuyanzhi3@huawei.com>
    Change-Id: I9204d1335a8995cbbc52e51884db908af8191d0b

---
+++ services/test/unittest/common_event_manager_service_branch_test/mock_access_token_helper.cpp
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "access_token_helper.h"
+
+namespace {
+    bool g_mockVerifyNativeTokenRet = true;
+    bool g_mockIsSystemAppRet = true;
+    bool g_mockIsDlpHapRet = true;
+    bool g_mockVerifyShellTokenRet = true;
+}
+
+void MockVerifyNativeToken(bool mockRet)
+{
+    g_mockVerifyNativeTokenRet = mockRet;
+}
+
+void MockIsSystemApp(bool mockRet)
+{
+    g_mockIsSystemAppRet = mockRet;
+}
+
+void MockIsDlpHap(bool mockRet)
+{
+    g_mockIsDlpHapRet = mockRet;
+}
+
+void MockVerifyShellToken(bool mockRet)
+{
+    g_mockVerifyShellTokenRet = mockRet;
+}
+
+using namespace OHOS::Security::AccessToken;
+
+namespace OHOS {
+namespace EventFwk {
+bool AccessTokenHelper::VerifyNativeToken(const AccessTokenID &callerToken)
+{
+    return g_mockVerifyNativeTokenRet;
+}
+
+bool AccessTokenHelper::IsSystemApp()
+{
+    return g_mockIsSystemAppRet;
+}
+
+bool AccessTokenHelper::IsDlpHap(const AccessTokenID &callerToken)
+{
+    return g_mockIsDlpHapRet;
+}
+
+bool AccessTokenHelper::VerifyShellToken(const AccessTokenID &callerToken)
+{
+    return g_mockVerifyShellTokenRet;
+}
+}  // namespace EventFwk
+}  // namespace OHOS
