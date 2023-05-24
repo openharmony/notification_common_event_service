d3e1bb73b8a2af4933d4071053e3ea978f1d2d93
xinking129 <xinxin13@huawei.com>
2023-05-24 17:53:23 +0800

    change file io to kv db
    Signed-off-by: xinking129 <xinxin13@huawei.com>

--- services/src/static_subscriber_data_manager.cpp
+++ services/src/static_subscriber_data_manager.cpp
@@ -15,114 +15,160 @@
 
 #include "static_subscriber_data_manager.h"
 
-#include <fstream>
 #include <unistd.h>
 
 #include "ces_inner_error_code.h"
-#include "directory_ex.h"
 #include "event_log_wrapper.h"
 
 namespace OHOS {
 namespace EventFwk {
 namespace {
-const static std::string DISABLE_STATIC_SUBSCRIBER_DATA_FULL_PATH(
-    "/data/service/el1/public/common_event_service/disablestaticsubscribe.db");
-}
-std::mutex StaticSubscriberDataManager::dataMutex_;
-
+constexpr int32_t CHECK_INTERVAL = 100000; // 100ms
+constexpr int32_t MAX_TIMES = 5;           // 5 * 100ms = 500ms
+constexpr const char *STATIC_SUBSCRIBER_STORAGE_DIR = "/data/service/el1/public/database/common_event_service";
+const std::string STATIC_SUBSCRIBER_VALUE_DEFAULT = "0";
+} // namespace
 StaticSubscriberDataManager::StaticSubscriberDataManager() {}
 
-StaticSubscriberDataManager::~StaticSubscriberDataManager() {}
+StaticSubscriberDataManager::~StaticSubscriberDataManager()
+{
+    if (kvStorePtr_ != nullptr) {
+        dataManager_.CloseKvStore(appId_, kvStorePtr_);
+    }
+}
+
+DistributedKv::Status StaticSubscriberDataManager::GetKvStore()
+{
+    DistributedKv::Options options = {
+        .createIfMissing = true,
+        .encrypt = false,
+        .autoSync = true,
+        .syncable = false,
+        .area = DistributedKv::EL1,
+        .kvStoreType = DistributedKv::KvStoreType::SINGLE_VERSION,
+        .baseDir = STATIC_SUBSCRIBER_STORAGE_DIR
+    };
+
+    DistributedKv::Status status = dataManager_.GetSingleKvStore(options, appId_, storeId_, kvStorePtr_);
+    if (status != DistributedKv::Status::SUCCESS) {
+        EVENT_LOGE("return error: %{public}d", status);
+    } else {
+        EVENT_LOGI("get kvStore success");
+    }
+    return status;
+}
+
+bool StaticSubscriberDataManager::CheckKvStore()
+{
+    if (kvStorePtr_ != nullptr) {
+        return true;
+    }
+    int32_t tryTimes = MAX_TIMES;
+    while (tryTimes > 0) {
+        DistributedKv::Status status = GetKvStore();
+        if (status == DistributedKv::Status::SUCCESS && kvStorePtr_ != nullptr) {
+            return true;
+        }
+        EVENT_LOGI("try times: %{public}d", tryTimes);
+        usleep(CHECK_INTERVAL);
+        tryTimes--;
+    }
+    return kvStorePtr_ != nullptr;
+}
 
 int32_t StaticSubscriberDataManager::InsertDisableStaticSubscribeData(const std::string &bundleName)
 {
-    EVENT_LOGD("bundleName: %{public}s", bundleName.c_str());
-
     if (bundleName.empty()) {
-        EVENT_LOGW("Invalid value!");
+        EVENT_LOGW("invalid value!");
         return ERR_INVALID_VALUE;
     }
 
-    std::set<std::string> disableStaticSubscribeAllData;
-    if ((QueryDisableStaticSubscribeAllData(disableStaticSubscribeAllData) == ERR_OK) &&
-        (!disableStaticSubscribeAllData.empty())) {
-        if (disableStaticSubscribeAllData.find(bundleName) != disableStaticSubscribeAllData.end()) {
-            EVENT_LOGI("Insert bundleName again!");
-            return ERR_OK;
-        }
-    }
+    EVENT_LOGD("bundleName: %{public}s", bundleName.c_str());
     {
-        std::lock_guard<std::mutex> lock(dataMutex_);
-        std::ofstream file;
-        file.open(DISABLE_STATIC_SUBSCRIBER_DATA_FULL_PATH.c_str(), std::ios::out | std::ios::app);
-        if (!file.is_open()) {
-            EVENT_LOGE("Failed to open file!");
-            return ERR_ENOUGH_DATA;
+        std::lock_guard<std::mutex> lock(kvStorePtrMutex_);
+        if (!CheckKvStore()) {
+            EVENT_LOGE("kvStore is nullptr");
+            return ERR_NO_INIT;
         }
-        file << bundleName << std::endl;
-        file.close();
     }
+
+    DistributedKv::Key key(bundleName);
+    DistributedKv::Value value(STATIC_SUBSCRIBER_VALUE_DEFAULT);
+    DistributedKv::Status status;
+    {
+        std::lock_guard<std::mutex> lock(kvStorePtrMutex_);
+        status = kvStorePtr_->Put(key, value);
+    }
+    if (status != DistributedKv::Status::SUCCESS) {
+        EVENT_LOGE("insert data to kvStore error: %{public}d", status);
+        return ERR_INVALID_OPERATION;
+    }
+
+    dataManager_.CloseKvStore(appId_, kvStorePtr_);
+    kvStorePtr_ = nullptr;
+
     return ERR_OK;
 }
 
 int32_t StaticSubscriberDataManager::DeleteDisableStaticSubscribeData(const std::string &bundleName)
 {
-    EVENT_LOGD("bundleName: %{public}s", bundleName.c_str());
-
     if (bundleName.empty()) {
-        EVENT_LOGW("Invalid value!");
+        EVENT_LOGW("invalid value!");
         return ERR_INVALID_VALUE;
     }
 
-    std::set<std::string> disableStaticSubscribeAllData;
-    if (QueryDisableStaticSubscribeAllData(disableStaticSubscribeAllData) == ERR_OK) {
-        if (disableStaticSubscribeAllData.find(bundleName) == disableStaticSubscribeAllData.end()) {
-            EVENT_LOGI("Delete bundleName is not exist!");
-            return ERR_OK;
+    EVENT_LOGD("bundleName: %{public}s", bundleName.c_str());
+    {
+        std::lock_guard<std::mutex> lock(kvStorePtrMutex_);
+        if (!CheckKvStore()) {
+            EVENT_LOGE("kvStore is nullptr");
+            return ERR_NO_INIT;
         }
     }
 
-    if (disableStaticSubscribeAllData.empty()) {
-        EVENT_LOGI("disableStaticSubscribeAllData is empty");
-        return ERR_OK;
-    }
-    disableStaticSubscribeAllData.erase(bundleName);
+    DistributedKv::Key key(bundleName);
+    DistributedKv::Status status;
     {
-        std::lock_guard<std::mutex> lock(dataMutex_);
-        std::ofstream file;
-        file.open(DISABLE_STATIC_SUBSCRIBER_DATA_FULL_PATH.c_str(), std::ios::out | std::ios::trunc);
-        if (!file.is_open()) {
-            EVENT_LOGE("Failed to open file!");
-            return ERR_ENOUGH_DATA;
-        }
-        for (std::string s : disableStaticSubscribeAllData) {
-            file << s << std::endl;
-        }
-        file.close();
+        std::lock_guard<std::mutex> lock(kvStorePtrMutex_);
+        status = kvStorePtr_->Delete(key);
     }
+    if (status != DistributedKv::Status::SUCCESS) {
+        EVENT_LOGE("delete data from kvStore error: %{public}d", status);
+        return ERR_INVALID_OPERATION;
+    }
+
+    dataManager_.CloseKvStore(appId_, kvStorePtr_);
+    kvStorePtr_ = nullptr;
+
     return ERR_OK;
 }
 
 int32_t StaticSubscriberDataManager::QueryDisableStaticSubscribeAllData(
     std::set<std::string> &disableStaticSubscribeAllData)
 {
-    std::lock_guard<std::mutex> lock(dataMutex_);
-    std::ifstream file;
-    file.open(DISABLE_STATIC_SUBSCRIBER_DATA_FULL_PATH.c_str(), std::ios::in);
-
-    if (!file.is_open()) {
-        EVENT_LOGE("Failed to open file!");
-        return ERR_NO_INIT;
+    {
+        std::lock_guard<std::mutex> lock(kvStorePtrMutex_);
+        if (!CheckKvStore()) {
+            EVENT_LOGE("kvStore is nullptr");
+            return ERR_NO_INIT;
+        }
     }
 
-    std::string data;
-    while (getline(file, data)) {
-        disableStaticSubscribeAllData.insert(data);
+    std::vector<DistributedKv::Entry> allEntries;
+    DistributedKv::Status status = kvStorePtr_->GetEntries(nullptr, allEntries);
+    if (status != DistributedKv::Status::SUCCESS) {
+        EVENT_LOGE("get entries error: %{public}d", status);
+        return ERR_INVALID_VALUE;
     }
-    for (std::string s : disableStaticSubscribeAllData) {
-        EVENT_LOGI("disableStaticSubscribeAllData[%{public}s].", s.c_str());
+
+    for (const auto &item : allEntries) {
+        disableStaticSubscribeAllData.emplace(item.key.ToString());
+        EVENT_LOGI("current disable static subscriber bundle name: %{public}s", item.key.ToString().c_str());
     }
-    file.close();
+
+    dataManager_.CloseKvStore(appId_, kvStorePtr_);
+    kvStorePtr_ = nullptr;
+
     return ERR_OK;
 }
 } // namespace EventFwk
