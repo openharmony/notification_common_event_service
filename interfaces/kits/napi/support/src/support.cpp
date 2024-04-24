6479e34ca8ce5e421c693b2da89b7e0133e4d76a
ligongshao <flamingsword1953@163.com>
2024-04-24 11:33:57 +0800

    add event COMMON_EVENT_SCREEN_LOCK_FILE_ACCESS_STATE_CHANGED
    Signed-off-by: ligongshao <flamingsword1953@163.com>
    Change-Id: I48a5824e8c1c640aa68decbd1d8f347dab3f5d01
    Signed-off-by: ligongshao <flamingsword1953@163.com>

--- interfaces/kits/napi/support/src/support.cpp
+++ interfaces/kits/napi/support/src/support.cpp
@@ -630,6 +630,10 @@ napi_value SupportInit(napi_env env, napi_value exports)
         obj,
         EventFwk::CommonEventSupport::COMMON_EVENT_PRIVACY_STATE_CHANGED,
         "COMMON_EVENT_PRIVACY_STATE_CHANGED");
+    SetNamedPropertyByStr(env,
+        obj,
+        EventFwk::CommonEventSupport::COMMON_EVENT_SCREEN_LOCK_FILE_ACCESS_STATE_CHANGED,
+        "COMMON_EVENT_SCREEN_LOCK_FILE_ACCESS_STATE_CHANGED");
     napi_property_descriptor exportFuncs[] = {DECLARE_NAPI_PROPERTY("Support", obj)};
     napi_define_properties(env, exports, sizeof(exportFuncs) / sizeof(*exportFuncs), exportFuncs);
 
