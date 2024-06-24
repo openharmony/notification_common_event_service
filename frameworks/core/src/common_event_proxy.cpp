b3fd53237f362cf1b63ede9abc0bd997e34ece1e
markYao <yaozhiyuan@huawei.com>
2024-06-24 19:10:56 +0800

    pidList解析时增加size校验
    Signed-off-by: markYao <yaozhiyuan@huawei.com>

--- frameworks/core/src/common_event_proxy.cpp
+++ frameworks/core/src/common_event_proxy.cpp
@@ -535,6 +535,11 @@ bool CommonEventProxy::SetFreezeStatus(std::set<int> pidList, bool isFreeze)
         return false;
     }
 
+    if (pidList.size() > VECTOR_MAX_SIZE) {
+        EVENT_LOGE("PidList size exceeds the max size.");
+        return ERR_NOTIFICATION_CES_COMMON_PARAM_INVALID;
+    }
+
     if (!data.WriteInt32(pidList.size())) {
         EVENT_LOGE("Failed to write int pidList size");
         return false;
