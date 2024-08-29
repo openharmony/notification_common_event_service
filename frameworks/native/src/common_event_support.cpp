ce365e6c900d35ca65128ffbcbf6a021789a780a
Angus <guomin2@huawei.com>
2024-08-29 22:50:09 +0800

    Add vpn connection event to support list
    Signed-off-by: Angus <guomin2@huawei.com>

--- frameworks/native/src/common_event_support.cpp
+++ frameworks/native/src/common_event_support.cpp
@@ -2809,6 +2809,12 @@ void CommonEventSupport::Init()
     * This is a protected common event that can only be sent by system.
     */
     commonEventSupport_.emplace_back(CommonEventSupport::COMMON_EVENT_DISPOSED_RULE_DELETED);
+
+    /**
+    * This common event means that vpn connection status has been changed.
+    * This is a protected common event that can only be sent by system.
+    */
+    commonEventSupport_.emplace_back(CommonEventSupport::COMMON_EVENT_VPN_CONNECTION_STATUS_CHANGED);
     return;
 }
 
