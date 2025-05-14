2cf2b690f7762e9643588b66cc94918035794175
lanhaoyu <lanhaoyu3@huawei-partners.com>
2025-05-14 16:43:14 +0800

    add shortcut changed
    Signed-off-by: lanhaoyu <lanhaoyu3@huawei-partners.com>

--- interfaces/kits/napi/support/src/support.cpp
+++ interfaces/kits/napi/support/src/support.cpp
@@ -674,6 +674,10 @@ napi_value SupportInit(napi_env env, napi_value exports)
         obj,
         EventFwk::CommonEventSupport::COMMON_EVENT_DEFAULT_APPLICATION_CHANGED,
         "COMMON_EVENT_DEFAULT_APPLICATION_CHANGED");
+    SetNamedPropertyByStr(env,
+        obj,
+        EventFwk::CommonEventSupport::COMMON_EVENT_SHORTCUT_CHANGED,
+        "COMMON_EVENT_SHORTCUT_CHANGED");
     napi_property_descriptor exportFuncs[] = {DECLARE_NAPI_PROPERTY("Support", obj)};
     napi_define_properties(env, exports, sizeof(exportFuncs) / sizeof(*exportFuncs), exportFuncs);
 
