35b61a47450c68b8a510978cadc02f03dae30ca3
cheerful_ricky <yuegang7@huawei.com>
2025-05-23 09:36:47 +0800

    fix OnReceiveEvent err not null issue
    Signed-off-by: cheerful_ricky <yuegang7@huawei.com>

--- interfaces/kits/ani/common_event/src/ani_common_event.cpp
+++ interfaces/kits/ani/common_event/src/ani_common_event.cpp
@@ -241,10 +241,11 @@ void SubscriberInstance::OnReceiveEvent(const CommonEventData& data)
     ani_object ani_data {};
     AniCommonEventUtils::ConvertCommonEventDataToEts(etsEnv, ani_data, data);
 
-    ani_object businessErrorObject;
-    int32_t code = 0;
-    std::string message = "";
-    AniCommonEventUtils::CreateBusinessErrorObject(etsEnv, businessErrorObject, code, message);
+    ani_ref nullObject;
+    aniResult = etsEnv->GetNull(&nullObject);
+    if (aniResult != ANI_OK) {
+        EVENT_LOGE("subscribeCallbackThreadFunciton GetNull error. result: %{public}d.", aniResult);
+    }
 
     auto fnObject = reinterpret_cast<ani_fn_object>(static_cast<ani_ref>(callback_));
     if (fnObject == nullptr) {
@@ -253,7 +254,7 @@ void SubscriberInstance::OnReceiveEvent(const CommonEventData& data)
     }
 
     EVENT_LOGI("FunctionalObject_Call.");
-    std::vector<ani_ref> args = { static_cast<ani_ref>(businessErrorObject), static_cast<ani_ref>(ani_data) };
+    std::vector<ani_ref> args = { nullObject, reinterpret_cast<ani_ref>(ani_data) };
     ani_ref result;
     aniResult = etsEnv->FunctionalObject_Call(fnObject, args.size(), args.data(), &result);
     if (aniResult != ANI_OK) {
