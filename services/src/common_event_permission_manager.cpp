a788f76203adb17594f5dda4b3abfd3c572b496f
gcw_8avsHzpw <hanjinghui2@huawei.com>
2026-05-12 21:45:24 +0800

    增加电话服务相关事件的权限限制 Co-Authored-By: Agent Signed-off-by: gcw_8avsHzpw <hanjinghui2@huawei.com>

--- services/src/common_event_permission_manager.cpp
+++ services/src/common_event_permission_manager.cpp
@@ -180,6 +180,13 @@ static const std::unordered_map<std::string, std::pair<PermissionState, std::vec
     {CommonEventSupport::COMMON_EVENT_VOLUME_ENCRYPTION_POLICY_SET,
         {PermissionState::DEFAULT, {"ohos.permission.QUERY_VOLUME_ENCRYPTION_STATUS"}}
     },
+    {CommonEventSupport::COMMON_EVENT_CALL_STATE_CHANGED,
+        {PermissionState::DEFAULT, {"ohos.permission.GET_TELEPHONY_STATE"}}
+    },
+    {CommonEventSupport::COMMON_EVENT_INCOMING_CALL_MISSED,
+        {PermissionState::DEFAULT, {"ohos.permission.GET_TELEPHONY_STATE"}}
+    },
+
 };
 
 static const std::unordered_set<std::string> SYSTEM_API_COMMON_EVENTS {
